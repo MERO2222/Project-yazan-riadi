d0b55a014d43df961e44f38c2b24edd6a8f92750
MERO2222 <106553042+MERO2222@users.noreply.github.com>
2022-05-30 21:34:16 +0300

    U

---
+++ main.js
@@ -0,0 +1,36 @@
+function eve () {
+  document.getElementById("a").style.display = "block"
+
+
+  
+} 
+function ev(){
+  document.getElementById("a").style.display = "none"
+}
+
+function eve2(){
+   document.getElementById("b").style.display = "block"
+}
+function ev2(){
+  document.getElementById("b").style.display = "none"
+} 
+function yt1(){
+  document.getElementById("Y").style.display = "block"
+} 
+function yt2(){
+  document.getElementById("Y").style.display = "none"
+}
+  
+function start(){
+  document.getElementById("ss").style.display = "block"
+  
+} 
+function sc(){
+  document.getElementById("ss").style.display = "none"
+} 
+function vso() {
+  document.getElementById("vss").style.display = "block"
+}
+function vsc(){
+  document.getElementById("vss").style.display = "none"
+} 
